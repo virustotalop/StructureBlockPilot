5a2f863cb3a980ca0e333cc408908b3e2e4cf225
virustotalop <virustotalop@gmail.com>
2021-11-20 01:52:00 -0800

    Cleanup imports

--- src/main/java/com/gmail/virustotalop/structureboxespilot/StructureBoxesPilot.java
+++ src/main/java/com/gmail/virustotalop/structureboxespilot/StructureBoxesPilot.java
@@ -1,18 +1,7 @@
 package com.gmail.virustotalop.structureboxespilot;
 
 import io.github.eirikh1996.structureboxes.compat.we6.IWorldEditHandler;
-import io.github.eirikh1996.structureboxes.StructureBoxes;
-import io.github.eirikh1996.structureboxes.localisation.I18nSupport;
-import io.github.eirikh1996.structureboxes.settings.Settings;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Map;
 
 public class StructureBoxesPilot extends JavaPlugin {
 
